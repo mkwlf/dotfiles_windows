3c0e117196e3f1ec4d517045ba9353534454122c
Michael Wolf <mike@mikewolf.me>
2022-02-17 13:12:34 +0100

    [zsh] adds pretty path printing

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,5 +1,8 @@
 alias ping-google="ping 8.8.8.8 -c 8"
-alias prp="pipenv run python"#
+alias prp="pipenv run python"
 
 # git
 alias gba="git branch -a | tee"
+
+# pretty-path
+alias pretty-path='echo -e ${PATH//:/\\n}'
